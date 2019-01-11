f06f5477807152043a58ba491a984dd09821ecc4
Mikołaj Stankiewicz <gilotyna815@gmail.com>
2019-01-11 14:25:06 +0100

    Added animations to doors, curtains; added colliders and grabbable and movable scripts to various objects
    Co-Authored-By: xiens <xiens@users.noreply.github.com>

--- Obiektyw60plus/Assets/Scripts/Wands/WandOfMoveFurniture.cs
+++ Obiektyw60plus/Assets/Scripts/Wands/WandOfMoveFurniture.cs
@@ -69,11 +69,11 @@ public class WandOfMoveFurniture : MonoBehaviour
 
 
         //TODO FIX this below (add the gameobject to scene with scripts)
-        //SomethingIsUsed = curtainController.Use || moveShelves.IsShelfSelected || move;
+        SomethingIsUsed = curtainController.Use || moveShelves.IsShelfSelected || move;
 
-        //if (IsSelectedShelf() && MovementInput > 0 && !SomethingIsUsed) moveShelves.IsShelfSelected = true; //moving shelves
-        //if (IsSelectedDoor() && MovementInput > 0 && !SomethingIsUsed) moveDoor.Move = true; //moving door
-        //if (IsSelectedCurtain() && MovementInput > 0 && !SomethingIsUsed) curtainController.Use = true; //using curtains
+        if (IsSelectedShelf() && MovementInput > 0 && !SomethingIsUsed) moveShelves.IsShelfSelected = true; //moving shelves
+        if (IsSelectedDoor() && MovementInput > 0 && !SomethingIsUsed) moveDoor.Move = true; //moving door
+        if (IsSelectedCurtain() && MovementInput > 0 && !SomethingIsUsed) curtainController.Use = true; //using curtains
 
         if (/*/*IsSelectedMovableObject() &&*/ MovementInput > 0) { move = true; }
         else { move = false; }
