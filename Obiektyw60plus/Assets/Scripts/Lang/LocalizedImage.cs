ad4b41f74095bb36a305dc5c5d742adfa523133f
Aleksandra Kotula <o.k.nameless@gmail.com>
2019-02-05 19:01:56 +0100

    Final OK documentation

--- Obiektyw60plus/Assets/Scripts/Lang/LocalizedImage.cs
+++ Obiektyw60plus/Assets/Scripts/Lang/LocalizedImage.cs
@@ -2,28 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>LocalizedImage</c> handles image change in case of language change.
+/// This script should be attached to every <c>Image</c> element that is supposed to differ in language versions. Element with this script should also be assign Lang tag.
+/// </summary>
 public class LocalizedImage : MonoBehaviour {
 
+    /// <summary>
+    /// Polish version of the image to be displayed.
+    /// </summary>
     public Sprite polishImage;
+    /// <summary>
+    /// English version of the image to be displayed.
+    /// </summary>
     public Sprite englishImage;
     
 
-	// Use this for initialization
+	/// <summary>
+    /// Load initial image.
+    /// </summary>
 	void Start () {
         ReloadImage();
 	}
 	
-
+    /// <summary>
+    /// Reload image depending on language value in <c>PlayerPrefs</c>
+    /// </summary>
 	public void ReloadImage () {
         if (PlayerPrefs.GetInt("lang") == 1)
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = polishImage;
-            print("Image should be polish");
+            //print("Image should be polish");
         }
         else
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = englishImage;
-            print("Image should be english");
+            //print("Image should be english");
         }
 	}
 }
--- Obiektyw60plus/Assets/Scripts/Lang/LocalizedText.cs
+++ Obiektyw60plus/Assets/Scripts/Lang/LocalizedText.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Class <c>LocalizedText</c> handles text change in case of language change.
+/// This script should be attached to every <c>Text</c> element that is supposed to differ in language versions. Element with this script should also be assign Lang tag.
+/// </summary>
 public class LocalizedText : MonoBehaviour {
 
+    /// <summary>
+    /// Text's key by which proper version is looked up in language files.
+    /// </summary>
     public string key;
+    /// <summary>
+    /// Text component.
+    /// </summary>
     Text text;
-    // Use this for initialization
+
+    /// <summary>
+    /// <c>LocalizedText</c> initialization.
+    /// </summary>
     void Start()
     {
         text = GetComponent<Text>();
         text.text = LocalizationManager.instance.GetLocalizedValue(key);
     }
+    /// <summary>
+    /// Reloading text value depending on text file loaded in <c>LocalizationManager</c>.
+    /// </summary>
     public void ReloadText()
     {
         text.text = LocalizationManager.instance.GetLocalizedValue(key);
